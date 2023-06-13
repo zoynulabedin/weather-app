72983815882b7940aa073bcc9a4ab2d7fea0c92f
Zoynul <33788041+zoynulabedin@users.noreply.github.com>
2023-06-13 08:39:28 +0600

    apit https added

--- src/features/weatherApi.js
+++ src/features/weatherApi.js
@@ -6,7 +6,7 @@ export const fetchWeatherData = createAsyncThunk(
 	"weather/fetchWeatherData",
 	async (location) => {
 		const response = await axios.get(
-			`http://api.weatherapi.com/v1/current.json?key=956cb11cf9834e72b1d54431230904&q=${location}&aqi=no`
+			`https://api.weatherapi.com/v1/current.json?key=956cb11cf9834e72b1d54431230904&q=${location}&aqi=no`
 		);
 		return response.data;
 	}
